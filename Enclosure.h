f2bd19898f374595cc926bb74ac9ac623de14581
omarrosama <omar.gamal@student.giu-uni.de>
2025-11-12 15:44:53 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,18 @@
 #ifndef ENCLOSURE_H
 #define ENCLOSURE_H
-
 #include "Animal.h"
-
-//define enclosure class here 
-
-
-
-
-
-
-
-
+class Enclosure {
+    private:
+    Animal* animal;
+    int capacity;
+    int currentcount;
+    public:
+    Enclosure();
+    ~Enclosure();
+    void setAnimal(Animal*);
+    Animal* getAnimal();
+    Enclosure(Animal* animal, int capacity);
+    void addanimal(Animal* animal);
+    void displayanimals();
+};
 #endif
