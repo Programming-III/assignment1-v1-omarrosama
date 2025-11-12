75ec63adedd69339f11c01a91404735cff5d947e
omarrosama <omar.gamal@student.giu-uni.de>
2025-11-12 15:44:34 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,13 @@
 #ifndef BIRD_H
 #define BIRD_H
-
-#include "Animal.h"
-
-//define class bird here 
-
-
-
-
-
-
-
-#endif
+class Bird :public Animal {
+private:
+    float wingspan;
+public:
+    Bird();
+    ~Bird();
+    Bird(string name, int age, float wingspan);
+    void display();
+    float getWingspan();
+};
+#endif //BIRD_H
